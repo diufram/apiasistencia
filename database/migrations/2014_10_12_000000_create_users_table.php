e6aa92f38f7f37735f44e924cfc59aa35da5653a
diufram <diufram007@gmail.com>
2023-11-30 02:11:09 -0400

    SegundoCommit

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->string('password');
             $table->char('tipo');
-            $table->integer('telefono')->nullable();
+            $table->char('telefono')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
